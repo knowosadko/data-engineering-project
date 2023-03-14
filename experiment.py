e94cab4182370fadaa474b87324d56d2b6e79b7f
Ubuntu <kmnowosadko@gmail.com>
2023-03-14 01:06:36 +0000

    Add experiment script.

--- experiment.py
+++ experiment.py
@@ -2,16 +2,40 @@ from pyspark.sql import SparkSession
 from operator import add
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk 
-nltk.download('vader_lexicon')
+# nltk.download('vader_lexicon')
 import re
+import time
+import json
+from tqdm import tqdm
 
-spark = SparkSession.builder\
-        .master("spark://192.168.2.186:7077") \
-        .appName("Group14_Application")\
-        .config("spark.cores.max", 4)\
-        .getOrCreate()
+def main():
+    paramlist = [1,2,4,6] 
+    results = {}
+    for param in tqdm(paramlist):
+        spark = SparkSession.builder\
+            .master("spark://192.168.2.186:7077") \
+            .appName(f'Group14_Experiment_exec_{param}')\
+            .config("spark.cores.max", param).getOrCreate()
+            # .config("spark.dynamicAllocation.enabled",False)\
+            # .config("spark.executor.instances", f'{param}')\
+            # .config("spark.executor.memory", "1g")\
+            # .config('spark.executor.cores', '1')\
+        time1 = task1(spark)
+        st = time.time()
+        task2(spark)
+        et = time.time()
+        time2 = et - st
+        st = time.time()
+        task3(spark)
+        et = time.time()
+        time3 = et - st
+        spark.stop()
+        results[param] = (time1, time2, time3)
+    with open('./experiment_results.json', 'w') as f:
+        json.dump(results, f)
         
-def task_1():
+        
+def task1(spark):
     ranges = [i/10 for i in range(-10,11,1)]
     
     def assign_num(value):
@@ -22,14 +46,15 @@ def task_1():
     
     sia = SentimentIntensityAnalyzer()
     data = spark.read.json("sample_data.json")
+    st = time.time()
     body = data.select("id","body","score")
     rdd2=body.rdd.map(lambda x: (x["id"],x["body"],x["score"] ,sia.polarity_scores(x['body'])["compound"]))
     results = rdd2.toDF(["id","body","score","sentiment"])
-    rdd3 = results.rdd.map(lambda x: 
-    (assign_num(x['sentiment']),1)).reduceByKey(add)
-    return rdd3.take(20)
+    rdd3 = results.rdd.map(lambda x: (assign_num(x['sentiment']),1)).reduceByKey(add)
+    et = time.time()
+    return et-st
 
-def Simons_task():
+def task3(spark):
     data = spark.read.json("sample_data.json")
     # Drop all columns except body and score
     data2 = data.select(data["body"], data["score"])
@@ -58,10 +83,12 @@ def Simons_task():
     # Save the data to a file
     #data2_score_based_df.write.csv("data2_score_based.csv")
 
-def task_2():
+def task2(spark):
     data = spark.read.json("sample_data.json")
     data1 = data.groupBy(data["subreddit"]).count()
     data2 = data1.orderBy("count", ascending=False)
-    return data.take(10)
+    return data2.take(10)
 
 
+if __name__ == '__main__':
+    main()
